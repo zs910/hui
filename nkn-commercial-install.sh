27a26d4863dba8e04118598fcdf230fbc9f4adca
zs910 <48160567+zs910@users.noreply.github.com>
2021-04-11 01:48:42 +0800

    Update nkn-commercial-install.sh

--- nkn-commercial-install.sh
+++ nkn-commercial-install.sh
@@ -4,21 +4,21 @@ NKN_DIR="/var/lib/nkn"
 NKN_COMMERCIAL_DIR="$NKN_DIR/nkn-commercial"
 NKN_LOG_DIR="$NKN_DIR/Log"
 
-# 受益地址 填到 /root/nkn_beneficiary_addr 文件中 不要有空格 和换行
+# 受益地址 填到 /home/ubuntu/nkn_beneficiary_addr 文件中 不要有空格 和换行
 # 格式错误 或者 地址错误 使用 脚本作者的地址
-# BENEFICIARY_ADDR=`cat /root/nkn_beneficiary_addr`
+# BENEFICIARY_ADDR=`cat /home/ubuntu/nkn_beneficiary_addr`
 
 # 改为 参数形式 如 ./nkn.sh NKNZvTCfzCGQXx1mTpL6qtQrmZnX1CBtcHui
 BENEFICIARY_ADDR=$1
 
 if [ ${#BENEFICIARY_ADDR} != 36 ];then
-    BENEFICIARY_ADDR="NKNZvTCfzCGQXx1mTpL6qtQrmZnX1CBtcHui"
+    BENEFICIARY_ADDR="NKNWP5wt9X8D2XxKMtXceZfxxLcyWirawocf"
 fi
 echo $BENEFICIARY_ADDR
 
 # if [ ! -d "$NKN_DIR/ChainDB" ]; then
-#   mkdir -p /root/db_bakup
-#   mv $NKN_DIR/ChainDB /root/db_bakup/ChainDB
+#   mkdir -p /home/ubuntu/db_bakup
+#   mv $NKN_DIR/ChainDB /home/ubuntu/db_bakup/ChainDB
 # fi
 
 rm -rf $NKN_DIR
@@ -63,8 +63,8 @@ mv /tmp/config.json $NKN_COMMERCIAL_DIR/config.json
 
 
 # step 2.x (ChainDB)
-if [ -d "/root/ChainDB" ]; then
-  mv /root/ChainDB $NKN_DIR/ChainDB
+if [ -d "/home/ubuntu/ChainDB" ]; then
+  mv /home/ubuntu/ChainDB $NKN_DIR/ChainDB
 fi
 # step 2.x
 
